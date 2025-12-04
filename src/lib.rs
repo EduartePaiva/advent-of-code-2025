db01b2d1be45568d4a27501ba435ddc2abe3cb22
Eduarte <74308809+EduartePaiva@users.noreply.github.com>
2025-12-04 11:14:12 -0300

    setting up project

---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+pub mod questions;
+
+use std::{fs::File, io::Read};
+
+pub fn read_question_file(question_number: u16) -> String {
+    let mut input: String = String::new();
+
+    let path = "./inputs/q".to_string() + question_number.to_string().as_str() + ".txt";
+
+    File::open(path)
+        .expect("error opening the file")
+        .read_to_string(&mut input)
+        .expect("Some error converting to string");
+    input
+}
---
+++ src/questions/q1.rs
@@ -0,0 +1,5 @@
+use crate::read_question_file;
+
+pub fn q1() {
+    let file = read_question_file(1);
+}
