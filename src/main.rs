535fb87be48b9a5476ba80280e83d78d2feb3801
Eduarte <74308809+EduartePaiva@users.noreply.github.com>
2025-12-08 10:34:18 -0300

    q4 part 1

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 #![allow(dead_code, unused_imports)]
 
 use advent_of_code_2025::{
-    questions::{q1, q2, q3},
+    questions::{q1, q2, q3, q4},
     read_question_file,
 };
 
@@ -10,6 +10,7 @@ fn main() {
     // println!("q1 pt 2 answer {}", q1::p2(read_question_file(1, 2)));
     // println!("q2 pt 1 answer {}", q2::p1(read_question_file(2, 1)));
     // println!("q2 pt 1 answer {}", q2::p2(read_question_file(2, 1)));
-    println!("q3 pt 1 answer {}", q3::p1(read_question_file(3, 1)));
-    println!("q3 pt 2 answer {}", q3::p2(read_question_file(3, 1)));
+    // println!("q3 pt 1 answer {}", q3::p1(read_question_file(3, 1)));
+    // println!("q3 pt 2 answer {}", q3::p2(read_question_file(3, 1)));
+    println!("q4 pt 1 answer {}", q4::p1(read_question_file(4, 1)));
 }
---
+++ src/questions/q4.rs
@@ -0,0 +1,90 @@
+fn is_roll_valid(grid: &Vec<Vec<char>>, r: usize, c: usize) -> bool {
+    let dirs = [
+        // up
+        [-1, 0],
+        // down
+        [1, 0],
+        // left
+        [0, -1],
+        // right
+        [0, 1],
+        // up+left
+        [-1, -1],
+        // up+right
+        [-1, 1],
+        // down+left
+        [1, -1],
+        // down+right
+        [1, 1],
+    ];
+
+    let r_size = grid.len() as i32;
+    let c_size = grid[0].len() as i32;
+
+    let mut good = 0;
+    for dir in dirs {
+        let new_r = r as i32 + dir[0];
+        let new_c = c as i32 + dir[1];
+
+        if new_r >= 0
+            && new_c >= 0
+            && new_r < r_size
+            && new_c < c_size
+            && grid[new_r as usize][new_c as usize] == '@'
+        {
+            good += 1;
+        }
+
+        if good > 3 {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+pub fn p1(input: String) -> u64 {
+    let mut total = 0;
+    let mut grid: Vec<Vec<char>> = vec![];
+
+    // parse the input into the grid
+    for line in input.lines() {
+        let mut row = vec![];
+        for c in line.chars() {
+            row.push(c);
+        }
+        grid.push(row);
+    }
+
+    for r in 0..grid.len() {
+        for c in 0..grid[0].len() {
+            if grid[r][c] == '@' && is_roll_valid(&grid, r, c) {
+                total += 1;
+            }
+        }
+    }
+
+    total
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test1() {
+        assert_eq!(
+            p1("..@@.@@@@.
+@@@.@.@.@@
+@@@@@.@.@@
+@.@@@@..@.
+@@.@@@@.@@
+.@@@@@@@.@
+.@.@.@.@@@
+@.@@@.@@@@
+.@@@@@@@@.
+@.@.@@@.@."
+                .to_string()),
+            13
+        );
+    }
+}
